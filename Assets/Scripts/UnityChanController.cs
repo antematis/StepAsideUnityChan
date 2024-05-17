725657ffd808b0b8f98a2cdd037acba124b391f5
json.ante <antematis@users.noreply.github.com>
2024-05-17 20:14:40 +0900

    あたり判定の追加

--- Assets/Scripts/UnityChanController.cs
+++ Assets/Scripts/UnityChanController.cs
@@ -17,6 +17,9 @@ public class UnityChanController : MonoBehaviour
     private float velocityY = 10f;
     //左右の移動できる範囲
     private float movableRange = 3.4f;
+    //動きを減速させる係数
+    private float coefficient = 0.99f;
+    private bool isEnd = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +34,14 @@ public class UnityChanController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ゲーム終了ならUnityちゃんの動きを減衰する
+        if (this.isEnd)
+        {
+            this.velocityZ *= this.coefficient;
+            this.velocityX *= this.coefficient;
+            this.velocityY *= this.coefficient;
+            this.myAnimator.speed *= this.coefficient;
+        }
         //横方向の入力による速度
         float inputVelocityX = 0;
         //上方向の入力による速度
@@ -68,5 +79,27 @@ public class UnityChanController : MonoBehaviour
         }
         //Unityちゃんに速度を与える
         this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, this.velocityZ);
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理
+    void OnTriggerEnter(Collider other)
+    {
+        //障害物に衝突した場合
+        if(other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
+        {
+            this.isEnd = true;
+        }
+        //ゴール地点に到達した場合
+        if(other.gameObject.tag == "GoalTag")
+        {
+            this.isEnd = true;
+        }
+
+        //コインに衝突した場合
+        if(other.gameObject.tag == "CoinTag")
+        {
+            //接触したコインのオブジェクトを破棄
+            Destroy(other.gameObject);
+        }
     }
 }
